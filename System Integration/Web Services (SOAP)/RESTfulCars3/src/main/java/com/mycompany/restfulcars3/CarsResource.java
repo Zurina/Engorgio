ddec2b22645c431042d2bb566b9dc6bd762de6ad
Mathias <Orchidevej@hotmail.com>
2019-09-23 17:37:57 +0200

    puta

--- System Integration/Web Services (SOAP)/RESTfulCars3/src/main/java/com/mycompany/restfulcars3/CarsResource.java
+++ System Integration/Web Services (SOAP)/RESTfulCars3/src/main/java/com/mycompany/restfulcars3/CarsResource.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.restfulcars2;
+package com.mycompany.restfulcars3;
 
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.JAXBException;
