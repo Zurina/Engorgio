c86ab73a294de6aba70a9665037e062304e0f523
Stanislav Novitski <Stanislav.Novitski@ibm.com>
2019-10-17 09:55:19 +0200

    da

--- Discreet Math/StaticAnalysis/src/staticanalysis/StaticAnalysis.java
+++ Discreet Math/StaticAnalysis/src/staticanalysis/StaticAnalysis.java
@@ -1,6 +1,5 @@
 package staticanalysis;
 
-
 public class StaticAnalysis {
 
     State state;
@@ -20,30 +19,25 @@ public class StaticAnalysis {
 //     LET X = 4711
 //     }
     public void analyseProgram() {
-        
+
         String[] lines = VSSL.split("\n");
         // Ved ikke omd det her giver mening, men så kan man også henvise til linjer før og efter hinanden
         // ved hjælp af i. fx når } er på en linje for sig, så kan man tjekke nogle linjer oppe om der er en linje
         // der slutter med {, så de to linjer ligesom danner et par :-D 
         for (int i = 0; i < lines.length; i++) {
             String VSSLLine = lines[i];
-            
+
             if (VSSLLine.startsWith("DEF")) {
-                
-            }
-            else if (VSSLLine.startsWith("LET")) {
-                
-            }
-            else if (VSSLLine.startsWith("IF")) {
-                
-            }
-            else if ((VSSLLine.startsWith("ELSE"))) {
-                
-            }
-            else if (VSSLLine.startsWith("}")) {
-                
-            }
-            else {
+
+            } else if (VSSLLine.startsWith("LET")) {
+
+            } else if (VSSLLine.startsWith("IF")) {
+
+            } else if ((VSSLLine.startsWith("ELSE"))) {
+
+            } else if (VSSLLine.startsWith("}")) {
+
+            } else {
                 System.out.println("SYNTAX INCORRECT PUTA!!!");
             }
         }
@@ -52,27 +46,36 @@ public class StaticAnalysis {
     private void definitionStatement(String nextVSSLLine) {
         String regex = "(DEF) ([A-Z])* : ((BOOLEAN)|(INTEGER))";
         if (nextVSSLLine.matches(regex)) {
-            
+
             nextVSSLLine = nextVSSLLine.substring(4);
-            
+
             String variableName = nextVSSLLine.split(":")[0];
             variableName = variableName.substring(0, variableName.length() - 1); // remove space between variable and :
             String variableType = nextVSSLLine.split(":")[1];
             variableType = variableType.substring(1); // remove space between : and type
-            
-            if (variableType.equals("INTEGER"))
+
+            if (variableType.equals("INTEGER")) {
                 state.defineInteger(variableName);
-            else
+            } else {
                 state.defineBoolean(variableName);
+            }
+        } else {
+            throw new Exception("The DEF syntax is invalid");
         }
     }
 
-    private void assignmentStatement() {
+    private void assignmentStatement(String nextVSSLLine) throws Exception {
+        String regex = "(LET) ([A-Z])* = ";
 
-        VSSL = VSSL.substring(VSSL.indexOf("LET") + 4);
-        String variableName = VSSL.split("=")[0];
-        String variableType = VSSL.split(" ")[1];
+        if (nextVSSLLine.matches(regex)) {
 
+            VSSL = VSSL.substring(VSSL.indexOf("LET") + 4);
+            String variableName = VSSL.split("=")[0];
+            String variableType = VSSL.split(" ")[1];
+
+        } else {
+            throw new Exception("The LET syntax is invalid");
+        }
     }
 
     private void ifStatement() {
